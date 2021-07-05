cbf85c24e872ca806bebf2f9d30e2bc0d787607f
Ana Júlia <anajuliavancan@gmail.com>
2021-07-05 13:21:58 +0000

    Classe Noticia com construtor e atributos adicionados.

---
+++ modelos/noticia.js
@@ -0,0 +1,8 @@
+class Noticia{
+  constructor(title, publishedAt, author, description){
+    this._title = title;
+    this._publishedAt = publishedAt;
+    this._author = author;
+    this._description = description;
+  }
+}
