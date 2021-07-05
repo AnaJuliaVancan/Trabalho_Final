5d5483889a15ec73cb6b06d4a5989e421354b6a4
Ana Júlia <anajuliavancan@gmail.com>
2021-07-05 13:51:23 +0000

    JavaScript adicionado

---
+++ script.js
@@ -0,0 +1,23 @@
+let requestURL = "https://www.luizpicolo.com.br/api.json";
+let XHM = new XMLHttpRequest();
+XHM.open('GET', requestURL);
+XHM.responseType = 'json';
+XHM.send();
+
+let elemento = document.getElementById('noticias');
+
+XHM.onload = function () {
+  let noticias = XHM.response;
+
+  noticias.forEach(function(noticia) {
+    let imagem = noticia.imagem;
+    let title = noticia.title;
+    let publishedAt = noticia.publishedAt;
+    let author = noticia.author;
+    let description = noticia.description
+
+    let n = new Noticia(title, publishedAt, author, description);
+
+    elemento.insertAdjacentHTML('afterbegin', n.mostrar)
+  })
+}
