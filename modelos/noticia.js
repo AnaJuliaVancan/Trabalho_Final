d1a655a2f34ee9cd1f322dff2da367dee3b7e014
Ana Júlia <anajuliavancan@gmail.com>
2021-07-09 12:57:39 +0000

    Conteúdo desnecessário retirado e algumas formatações para a página adicionadas.

--- modelos/noticia.js
+++ modelos/noticia.js
@@ -1,5 +1,5 @@
 class Noticia {
-  constructor(title, publishedAt, author, description, url, content) {
+  constructor(title, publishedAt, author, description, url) {
     this._title = title;
     this._publishedAt = publishedAt;
     this._author = author;
@@ -18,17 +18,29 @@ get mostrarNoticia() {
   }
 }
 
-mostrarNoticia(){
-  if (this._title != " " && this._author != " " && this._description != " ") {
-
-    return `
-        <h1>${this._title}</h1>
-        <div>${this._publishedAt}</div>
-        <div>${this._author}</div><br>
-        <div>${this._description}</div>
-        <div>${this._content}</div>
-      `;
-
+  aparecerNoticia() {
+    if (this._title != " " && this._author != " " && this._description != " " && this._publishedAt != " " && this._url != " ") {
+      return `
+            <div class="row p-3">
+               <div class="accordion" id="metodos" style="bacground-color">
+                    <div class="accordion-item">
+                     <h2 class="accordion-header">
+                         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" 
+                         data-bs-target="#item1">
+                              <h3 class="font-family-base" style="font-family: serif"><a href="${this._url}">${this._title}</a></h3>
+                         </button>
+                     </h2>
+                     <div id="item1" class="accordion-collapse collapse" data-bs-parent="#metodos">
+                         <div class="accordion-body">
+                               <div style="font-family: arial">${this._publishedAt}</div>
+                               <div style="font-family: arial">${this._author}</div><br>
+                               <div style="font-family: arial">${this._description}</div>
+                         </div>
+                     </div>
+                 </div>
+              </div>
+            </div>
+      `
   } else {
     throw new ErroCustomizado("Algo está vazio!Cheque se titulo, resumo ou texto estão preenchidos!!!");
   }
